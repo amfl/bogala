31f058ffa8427c1a99161b35adf6a83a078c1ae0
amfl <amfl@none.none>
2022-01-02 13:10:20 +1300

    Use honeycomb to draw grid

--- script.js
+++ script.js
@@ -6,19 +6,41 @@ const data = [
     { color: 2, x: 1, y: 0 }
 ];
 
+// Generate a grid with the Honeycomb library
+const Grid = Honeycomb.defineGrid();
+// const grid = Grid.rectangle({ width: 4, height: 4 });
+const grid = Grid.hexagon({ radius: 3 });
+console.log(grid);
+
 /////////////////////////////////////////////////////////
+
+const svg = document.getElementById('main');
 
 const hexbin = d3.hexbin();
 
+// Draw the board
 d3.select("svg")
-    .selectAll('path')
-    .data(data)
+    .selectAll('.grid')
+    .data(grid)
     .enter()
     .append('path')
-    .attr('transform', d => `translate(${d.x}, ${d.y})`)
+    .classed('grid', true)
+    .attr('transform', d => {
+        const c = d.toPoint();
+        return `translate(${c.x}, ${c.y})`
+    })
     .attr('d', hexbin.hexagon())
-    .attr('fill', d => colors[d.color])
-    .attr('stroke', 'black')
+    // .attr('fill', d => colors[d.color])
+    .attr('fill', 'lightgrey')
+    .attr('stroke', 'white')
     .attr('stroke-width', '0.1');
+
+// // No idea why this doesn't render anything.
+// const mesh = document.createElement('path');
+// mesh.setAttribute('d', hexbin.mesh());
+// mesh.setAttribute('stroke', 'black');
+// mesh.setAttribute('stroke-width', '0.1');
+// mesh.setAttribute('fill', 'red');
+// svg.appendChild(mesh);
 
 console.log("All done");
