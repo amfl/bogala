fe414335e9b56ff526cd65f0df9d7c5083f05899
amfl <amfl@none.none>
2022-01-02 13:19:36 +1300

    Draw pieces too

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const colors = [ "black", "white", "red", "yellow", "blue", "green" ];
 
-const data = [
-    { color: 0, x: 3, y: 2 },
+const pieces = [
+    { color: 0, x: 2, y: 2 },
     { color: 1, x: 0, y: 0 },
     { color: 2, x: 1, y: 0 }
 ];
@@ -34,6 +34,24 @@ d3.select("svg")
     .attr('fill', 'lightgrey')
     .attr('stroke', 'white')
     .attr('stroke-width', '0.1');
+
+// Draw the pieces
+d3.select("svg")
+    .selectAll('.piece')
+    .data(pieces)
+    .enter()
+    .append('circle')
+    .classed('piece', true)
+    .attr('transform', d => {
+        // Figure out where Honeycomb would stick this hex
+        const h = Grid.Hex(d.x, d.y);
+        const c = h.toPoint();
+        return `translate(${c.x}, ${c.y})`
+    })
+    .attr('fill', d => colors[d.color])
+    .attr('stroke', 'black')
+    .attr('stroke-width', '0.1')
+    .attr('r', '0.7');
 
 // // No idea why this doesn't render anything.
 // const mesh = document.createElement('path');
