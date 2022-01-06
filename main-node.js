a1000a26b348ba9f39f990a89b611ef836eb1e92
amfl <amfl@none.none>
2022-01-06 21:03:02 +1300

    Accept input from stdin. Write to file.

--- main-node.js
+++ main-node.js
@@ -2,6 +2,9 @@ import {parse} from "./bogala-lib.js";
 import { createSVGWindow } from "svgdom";
 import { SVG, registerWindow } from "@svgdotjs/svg.js";
 
+import process from 'process';
+import * as fs from 'fs';
+
 // returns a window with a document and an svg root node
 const window = createSVGWindow()
 const document = window.document
@@ -9,33 +12,23 @@ const document = window.document
 // register window and document
 registerWindow(window, document)
 
-const bogalaCode = `
-board hexhex 5
-s -2 -4 1
-s -1 -4 1,0,1
-s 1 -4  0,1,0
-
-s -3 -2 0
-
-s -1 -1 2
-s  1 -1 0
-
-s -2 0 0
-s 1 0 1
-s 2 0 2
-
-s -2 1 2
-
-s 0 2 1,0,1
-s 2 2 1,0
-
-s -1 4 1
-s 0 4 0,1,0
-s 1 4 1
-`
+var stdinBuffer = fs.readFileSync(0); // STDIN_FILENO = 0
+const bogalaCode = stdinBuffer.toString();
 
 const svgNode = document.documentElement
 parse(bogalaCode, svgNode);
 
 console.log(svgNode.outerHTML)
 console.log("Done");
+
+console.log(process.argv);
+if (process.argv.length > 2) {
+    let filename = process.argv[2];
+    fs.writeFile(filename, svgNode.outerHTML, err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      //file written successfully
+    })
+}
